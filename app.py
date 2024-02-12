a28099eac4b9e57ea356895b5ac7784fa79c3db7
Mohit_100 <mohithedaoo2000@gmail.com>
2024-02-12 17:39:43 +0530

    Update

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome to another Flask example"
+
+@app.route('/cal',methods=["GET"])
+def math_operation():
+    # here we will take inputs from POSTMAN and so we have written request.json
+    operation = request.json["operation"]
+    num1 = request.json["num1"]
+    num2 = request.json["num2"]
+    # above inputs we will get from HTML or Postman and then we will process the data
+
+    if operation == "add":
+        result = num1 + num2
+
+    elif operation == "multiply":
+        result = num1*num2
+
+    elif operation == "division":
+        result = num1/num2
+    
+    else:
+        result = num1 - num2
+
+    return result
+
+print(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True,port='5001')
